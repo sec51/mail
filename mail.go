7d6edae333cf25618a86b0e5c4278ebebb2b01f3
silenteh <silenteh@gmail.com>
2016-12-12 21:10:24 +0100

    Made the mail client more modular

--- mail.go
+++ mail.go
@@ -3,40 +3,53 @@ package mail
 import (
 	"crypto/tls"
 	"errors"
+
 	"github.com/sec51/goconf"
+
 	gomail "gopkg.in/gomail.v2"
 )
 
-const (
-	FROM_EMAIL  = "Falcon <info@sec51.com>"
-	SMTP_SERVER = "mail.blackdefense.com"
-	SMTP_PORT   = 587
-)
+type EmailConfig struct {
+	// The hostname or ip address of the mail server
+	Server string
 
-func Send(body, to, subject string) error {
+	// The mail server submission port
+	Port int
+
+	// the From email: can be in the form: "Info <info@sec51.com>"
+	From string
+
+	// The username to authenticate
+	Username string
+
+	// The password to authenticate
+	Password string
+}
+
+func (c *EmailConfig) Send(body, to, subject string) error {
 
 	m := gomail.NewMessage()
-	m.SetHeader("From", FROM_EMAIL)
+	m.SetHeader("From", c.From)
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body)
 
-	smtpServer := goconf.AppConf.String("smtp.server")
+	smtpServer := goconf.AppConf.String("smtp::server")
 	if smtpServer == "" {
 		return errors.New("SMTP server not configured,set: smtp.server = HOSTNAME in the config file")
 	}
 
-	smtpUser := goconf.AppConf.String("smtp.user")
+	smtpUser := goconf.AppConf.DefaultString("smtp::username", c.Username)
 	if smtpUser == "" {
-		return errors.New("SMTP user not configured,set: smtp.user = NAME in the config file")
+		return errors.New("SMTP username not configured, set: smtp::username = NAME in the config file")
 	}
 
-	smtpPass := goconf.AppConf.String("smtp.pass")
-	if smtpUser == "" {
-		return errors.New("SMTP password not configured,set: smtp.pass = PASSWORD in the config file")
+	smtpPass := goconf.AppConf.DefaultString("smtp::password", c.Password)
+	if smtpPass == "" {
+		return errors.New("SMTP password not configured, set: smtp::password = PASSWORD in the config file")
 	}
 
-	smtpPort := goconf.AppConf.DefaultInt("smtp.port", SMTP_PORT)
+	smtpPort := goconf.AppConf.DefaultInt("smtp::port", c.Port)
 
 	d := gomail.NewPlainDialer(smtpServer, smtpPort, smtpUser, smtpPass)
 	d.SSL = true
@@ -49,4 +62,3 @@ func Send(body, to, subject string) error {
 	// Send the email to Bob, Cora and Dan
 	return d.DialAndSend(m)
 }
-

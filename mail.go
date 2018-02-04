d4a3c53a0143dab94c93109749671711d7e62814
silenteh <silenteh@gmail.com>
2018-02-04 19:32:15 +0100

    Switched to Glide vendoring and added possibility to disable SMTP authentication

--- mail.go
+++ mail.go
@@ -24,41 +24,80 @@ type EmailConfig struct {
 
 	// The password to authenticate
 	Password string
+
+	// whether to make a TLS connection
+	UseTLS bool
+
+	// whether to use username and password for authentication
+	UseAUTH bool
 }
 
 func (c *EmailConfig) Send(body, to, subject string) error {
 
 	m := gomail.NewMessage()
-	m.SetHeader("From", c.From)
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body)
 
-	smtpServer := goconf.AppConf.String("smtp::server")
+	from := c.From
+	if from == "" {
+		from = goconf.AppConf.String("smtp::from")
+		if from == "" {
+			return errors.New("From email address not configured - set: smtp::from = FROM in the config file")
+		}
+	}
+
+	m.SetHeader("From", from)
+
+	smtpServer := c.Server
 	if smtpServer == "" {
-		return errors.New("SMTP server not configured,set: smtp.server = HOSTNAME in the config file")
+		smtpServer = goconf.AppConf.String("smtp::server")
+		if smtpServer == "" {
+			return errors.New("SMTP server not configured - set: smtp::server = HOSTNAME in the config file")
+		}
 	}
 
-	smtpUser := goconf.AppConf.DefaultString("smtp::username", c.Username)
+	smtpUser := c.Username
 	if smtpUser == "" {
-		return errors.New("SMTP username not configured, set: smtp::username = NAME in the config file")
+		smtpUser = goconf.AppConf.DefaultString("smtp::username", c.Username)
+		if smtpUser == "" {
+			//return errors.New("SMTP username not configured, set: smtp::username = NAME in the config file")
+		}
 	}
 
-	smtpPass := goconf.AppConf.DefaultString("smtp::password", c.Password)
+	smtpPass := c.Password
 	if smtpPass == "" {
-		return errors.New("SMTP password not configured, set: smtp::password = PASSWORD in the config file")
+		smtpPass = goconf.AppConf.DefaultString("smtp::password", c.Password)
+		if smtpPass == "" {
+			//return errors.New("SMTP password not configured, set: smtp::password = PASSWORD in the config file")
+		}
 	}
 
-	smtpPort := goconf.AppConf.DefaultInt("smtp::port", c.Port)
-
-	d := gomail.NewPlainDialer(smtpServer, smtpPort, smtpUser, smtpPass)
-	d.SSL = true
-	tlsconfig := &tls.Config{
-		InsecureSkipVerify: false,
-		ServerName:         smtpServer,
+	smtpPort := c.Port
+	if smtpPort == 0 {
+		smtpPort = goconf.AppConf.DefaultInt("smtp::port", c.Port)
+		if smtpPort == 0 {
+			return errors.New("SMTP port not configured, set: smtp::port = PORT in the config file")
+		}
 	}
-	d.TLSConfig = tlsconfig
 
-	// Send the email to Bob, Cora and Dan
+	if c.UseAUTH {
+		d := gomail.NewPlainDialer(smtpServer, smtpPort, smtpUser, smtpPass)
+		d.SSL = c.UseTLS
+
+		if d.SSL {
+			tlsconfig := &tls.Config{
+				InsecureSkipVerify: false,
+				ServerName:         smtpServer,
+			}
+			d.TLSConfig = tlsconfig
+		}
+
+		// Send the email to Bob, Cora and Dan
+		return d.DialAndSend(m)
+	}
+
+	d := gomail.Dialer{Host: smtpServer, Port: smtpPort}
 	return d.DialAndSend(m)
+
 }
